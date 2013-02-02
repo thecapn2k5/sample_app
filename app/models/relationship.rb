91af5674d4c41e3ed1e3d2983ac938ade545dbc7
Jew <thecapn.morgan@gmail.com>
2013-02-02 10:55:41 +0200

    Annotations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer          not null, primary key
+#  follower_id :string(255)
+#  integer     :string(255)
+#  followed_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Relationship < ActiveRecord::Base
    attr_accessible :followed_id
 
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :integer          not null, primary key
+#  follower_id :string(255)
+#  integer     :string(255)
+#  followed_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'spec_helper'
 
 describe Relationship do
